8d9ea5228706cc1b0de498c8d392b0306b9e1ba9
vimalkaur1981 <kaurvimal81@gmail.com>
2025-12-16 01:21:52 +0800

    remove redundant files

--- infra/terraform/Option-1/iam.tf
+++ infra/terraform/Option-1/iam.tf
@@ -15,3 +15,24 @@ resource "aws_iam_role_policy_attachment" "ecs_policy" {
   role       = aws_iam_role.iot_ecs_task_execution.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+
+resource "aws_iam_role" "github_actions" {
+  name = "github-actions-ecr-ecs"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = "arn:aws:iam::255945442255:oidc-provider/token.actions.githubusercontent.com"
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringLike = {
+          "token.actions.githubusercontent.com:sub" = "repo:<GITHUB_ORG>/<REPO_NAME>:*"
+        }
+      }
+    }]
+  })
+}
